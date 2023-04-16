e50bbf62cc885e518456aafeaf8f53eb5a5ccf37
AlpineAlbino <107396479+AlpineAlbino@users.noreply.github.com>
2023-04-16 19:21:00 +0700

    Update index.js
    Added comments and cleared redundant newlines

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
+// external packages
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, emotes } = require('./config.json');
-const discUtils = require('./utils/disc.js');
 const process = require('process');
 
+// native files
+const { prefix, emotes } = require('./config.json');
+const discUtils = require('./utils/disc.js');
 const keepAlive = require("./server.js");
 
+// detect presences, members, messages and reactions
 const client = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_PRESENCES, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_BANS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_WEBHOOKS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.DIRECT_MESSAGES]
 });
 
-// cooldowns
+// command cooldowns
 client.cooldowns = new Discord.Collection();
 
 // games need their own Collections
 client.worded = new Discord.Collection();
 client.cashier = new Discord.Collection();
 client.bp = new Discord.Collection();
-
-// typetest
 client.typeTest = new Discord.Collection();
 
+// commands (execute, read info, etc.)
 client.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('./commands');
 
+// pull out commands from respective folders and add them to Todd
 for (const folder of commandFolders) {
   const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
   for (const file of commandFiles) {
@@ -32,11 +35,11 @@ for (const folder of commandFolders) {
   }
 }
 
+// we're rolling!
 client.once('ready', () => {
   console.log('Ready!');
 
-  // console logs every five minutes to keep it alive
-  const pingerInterval = setInterval(pinger, 60 * 5 * 1000);
+  const pingerInterval = setInterval(pinger, 60 * 5 * 1000); // console logs every five minutes to keep it alive
   function pinger() {
     const message = `Still here, uptime ${Math.floor(process.uptime())}`;
     console.log(message);
@@ -45,20 +48,18 @@ client.once('ready', () => {
   }
 });
 
+// whenever a message is sent
 client.on('messageCreate', message => {
-  // check if it has prefix, in DMs, or not from a bot
+  // check if it has prefix, in DMs, or not from a bot: we don't take these
   if (!message.content.startsWith(`${prefix}`) || message.author.bot || message.channel.type === "DM") return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
-
-  const command = client.commands.get(commandName)
-    || client.commands.find(cmd => cmd.alias && cmd.alias.includes(commandName));
-
+  const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.alias && cmd.alias.includes(commandName));
   if (!command) return;
 
   // check if user is RAVEN
-  if (command.ownerOnly && message.author.id !== '642307176228061184') return message.reply(`${emotes.toddYell} H- hey! You're not Raven! Give me back my revolver!`);
+  if (command.ownerOnly && message.author.id !== '642307176228061184') return message.reply(`${emotes.toddYell} H- hey! You're not Raven! Hand over my revolver!`);
 
   // cooldown check
   if (message.author.id !== '642307176228061184') {
@@ -73,7 +74,6 @@ client.on('messageCreate', message => {
 
     if (timestamps.has(message.author.id)) {
       const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
       if (now < expirationTime) {
         const timeLeft = (expirationTime - now) / 1000;
         return message.reply(`${emotes.toddSurprised} Slow down, chief, those bullets can only be reloaded in **${timeLeft.toFixed(1)}s**.`);
@@ -101,5 +101,6 @@ client.on('messageCreate', message => {
 
 });
 
+// keep my baby alive
 keepAlive();
 client.login(process.env.TOKEN);
